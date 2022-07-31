dc5fac38210b5b3a066674dc53fcb1ae7d02e5a4
Oladipo Omotosho <65185499+Urahara-Kenpachi@users.noreply.github.com>
2022-07-31 21:47:30 +0100

    width adjusted

--- game.js
+++ game.js
@@ -44,12 +44,19 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   console.log(availableQuestions);
-    getNewQuestion();
+  getNewQuestion();
 };
 
-getNewQuestion = ( ) =>{
-questionCounter++;
+getNewQuestion = () => {
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-}
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText =currentQuestion["choice" + number];
+  })
+};
 
 startGame();
